9d297a532dedd8a124c6dd656049429041470084
highlifeintueten <m.schwartz@datingcafe.de>
2013-12-17 17:30:06 +0100

    ref #6154 Implemented tests for wildcard search requests.

--- test/integration/org/grails/plugins/elasticsearch/ElasticSearchServiceIntegrationSpec.groovy
+++ test/integration/org/grails/plugins/elasticsearch/ElasticSearchServiceIntegrationSpec.groovy
@@ -1,6 +1,7 @@
 package org.grails.plugins.elasticsearch
 
 import grails.plugin.spock.IntegrationSpec
+import org.apache.log4j.Logger
 import org.elasticsearch.action.admin.cluster.state.ClusterStateRequestBuilder
 import org.elasticsearch.client.AdminClient
 import org.elasticsearch.client.ClusterAdminClient
@@ -13,7 +14,7 @@ import test.Product
 
 class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
 
-    def elasticSearchService
+    ElasticSearchService elasticSearchService
     def elasticSearchAdminService
     def elasticSearchHelper
 
@@ -22,6 +23,20 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
      * Be aware of this when indexing new objects.
      */
 
+    def setupSpec() {
+        def product01 = new Product(name: "horst", price: 3.95)
+        product01.save(failOnError: true)
+
+        def product02 = new Product(name: "hobbit", price: 5.99)
+        product02.save(failOnError: true)
+
+        def product03 = new Product(name: "best", price: 10.99)
+        product03.save(failOnError: true)
+
+        def product04 = new Product(name: "high and supreme", price: 45.50)
+        product04.save(failOnError: true)
+    }
+
     def cleanupSpec() {
         def dataFolder = new File('data')
         if (dataFolder.isDirectory()) {
@@ -29,13 +44,12 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         }
     }
 
-    def "Index and un-index a domain object"() {
+    void "Index and un-index a domain object"() {
         given:
         def product = new Product(name: "myTestProduct")
         product.save(failOnError: true)
 
         when:
-        elasticSearchService.index(product)
         elasticSearchAdminService.refresh() // Ensure the latest operations have been exposed on the ES instance
 
         and:
@@ -49,7 +63,7 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         elasticSearchService.search("myTestProduct", [indices: Product, types: Product]).total == 0
     }
 
-    def "Indexing the same object multiple times updates the corresponding ES entry"() {
+    void "Indexing the same object multiple times updates the corresponding ES entry"() {
         given:
         def product = new Product(name: "myTestProduct")
         product.save(failOnError: true)
@@ -99,7 +113,7 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         searchResults[0].date == product.date
     }
 
-    void "a geo point location is marshalled and de-marshalled correctly"() {
+    def "a geo point location is marshalled and de-marshalled correctly"() {
         given:
         def location = new GeoPoint(
             lat: 53.00,
@@ -125,7 +139,7 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         searchResults[0].location == location
     }
 
-    void "a geo point is mapped correctly"() {
+    def "a geo point is mapped correctly"() {
 
         given:
         def location = new GeoPoint(
@@ -155,7 +169,7 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         return indexMetaData.mapping(typeName)
     }
 
-    void "search with geo distance filter"() {
+    def "search with geo distance filter"() {
         given: "a building with a geo point location"
         GeoPoint geoPoint = new GeoPoint(
             lat: 50.1,
@@ -191,10 +205,10 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         searchResults[0].id == building.id
     }
 
-    void "searching with filtered query"() {
+    def "searching with filtered query"() {
         given: "some products"
-        def wurstProduct = new Product(name: "wurst", price: 2.00)
-        wurstProduct.save(failOnError: true)
+        def wurmProduct = new Product(name: "wurm", price: 2.00)
+        wurmProduct.save(failOnError: true)
 
         def hansProduct = new Product(name: 'hans', price: 0.5)
         hansProduct.save(failOnError: true)
@@ -202,7 +216,7 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
         def fooProduct = new Product(name: 'foo', price: 5.0)
         fooProduct.save(failOnError: true)
 
-        elasticSearchService.index(wurstProduct, hansProduct, fooProduct)
+        elasticSearchService.index(wurmProduct, hansProduct, fooProduct)
         elasticSearchAdminService.refresh()
 
         when: "that a range filter find the product"
@@ -212,15 +226,89 @@ class ElasticSearchServiceIntegrationSpec extends IntegrationSpec {
             }
         })
 
-        then: "the result should be product 'wurst'"
+        then: "the result should be product 'wurm'"
         result.total == 1
         List<Product> searchResults = result.searchResults
-        searchResults[0].name == wurstProduct.name
+        searchResults[0].name == wurmProduct.name
+    }
+
+    void "searching with wildcards in query at first position"() {
+        when: "search with asterisk at first position"
+
+        Map params = [indices: Product, types: Product]
+        def result = elasticSearchService.search({
+            wildcard("name": "*st")
+        }, params)
+
+        then: "the result should contain 2 products"
+        result.total == 2
+        List<Product> searchResults = result.searchResults
+        searchResults*.name.containsAll('best', 'horst')
+    }
+
+    void "searching with wildcards in query at last position"() {
+        when: "search with asterisk at last position"
+
+        Map params2 = [indices: Product, types: Product]
+        def result2 = elasticSearchService.search({
+            wildcard("name": "ho*")
+        }, params2)
+
+        then: "the result should return 2 products"
+        result2.total == 2
+        List<Product> searchResults2 = result2.searchResults
+        searchResults2*.name.containsAll('horst', 'hobbit')
+    }
+
+    void "searching with wildcards in query in between position"() {
+        when: "search with asterisk in between position"
+
+        Map params3 = [indices: Product, types: Product]
+        def result3 = elasticSearchService.search({
+            wildcard("name": "s*eme")
+        }, params3)
+
+        then: "the result should return 1 product"
+        result3.total == 1
+        List<Product> searchResults3 = result3.searchResults
+        searchResults3[0].name == 'high and supreme'
+    }
+
+
+    void "searching for special characters in data pool"() {
+
+        given: "some products"
+        def product01 = new Product(name: "ästhätik", price: 3.95)
+        product01.save(failOnError: true)
+
+        elasticSearchService.index(product01)
+        elasticSearchAdminService.refresh()
+
+        when: "search for 'a umlaut' "
+
+        def result = elasticSearchService.search({
+            match("name": "ästhätik")
+        })
+
+        then: "the result should contain 1 product"
+        result.total == 1
+        List<Product> searchResults = result.searchResults
+        searchResults[0].name == product01.name
+    }
+
+    void "At the start of a test method the index should be empty."() {
+        when: 'unindex is called'
+        elasticSearchService.unindex([:])
+        elasticSearchAdminService.refresh()
+
+        then: 'the index should be empty'
+        !elasticSearchService.search(null as Closure, [:]).total
     }
 
     void "a search with one kilometer distance to postal code 80331 finds nothing"() {}
 
-    void "a search with five kilometers distance to postal code 80331 finds one location with postal code 81667"() {}
+    void "a search with five kilometers distance to postal code 80331 finds one location with postal code 81667"() {
+    }
 
     void "a search with ten kilometers distance to postal code 80331 finds locations with postal codes 81667 and 85774"() {
     }
